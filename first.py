5cd632e54b8b61cdd8a8be4e49b54ad019479178
h-avatar <hoseinkazemi1003@gmail.com>
2018-02-09 14:44:50 +0330

    test

--- first.py
+++ first.py
@@ -3,12 +3,4 @@ num = input("Please enter a number: ")
 if int(num) % 2 == 0:
     print("Even")
 else:
-<<<<<<< HEAD
     print("Odd")
-	
-/* hello world */
-
-num2 = input("please enter end number: ")
-=======
-    print("Odd")
->>>>>>> parent of 64a72a6... initail first commit
